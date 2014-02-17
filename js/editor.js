05a22dda5677f9111ec0a578ab4c1907e0c6f304
villene <laipnieks.vilnis@gmail.com>
2014-02-17 11:34:21 +0200

    Editors
    Klikšķina uz rūtiņām un atmiņā uzģenerē XMLu (cerams)

---
+++ js/editor.js
@@ -0,0 +1,77 @@
+var game = new Phaser.Game(800, 500, Phaser.AUTO, '', { preload: preload, create: create});
+
+function preload() {
+        game.load.spritesheet('stance', 'assets/download.png', 20, 20, 2);
+}
+var gridHeight = 25;
+var gridWidth = 40;
+
+var note=[];
+var xmlNotes=[];
+function create() {
+	for (var j = 0; j<gridHeight; j++){
+            note[j]=[];
+        for (var i = 0; i < gridWidth; i++)
+    {
+        note[j][i] = game.add.button(i*20, j*20, 'stance', fillNote, note[j][i], 1,0,1);
+        note[j][i].hgt=j;
+        note[j][i].wdt=i;
+        note[j][i].on=false;
+    }
+	}
+}
+function fillNote()
+{
+    var y=this.hgt;
+    var x=this.wdt;
+    
+    for (var i = 0; i<gridHeight; i++)
+        {
+            if(i===y)continue;
+            if (note[i][x].on){note[i][x].on=false;
+            note[i][x].setFrames(1, (note[i][x].on)?1:0, 1);
+            note[i][x].frame = (note[i][x].on)?1:0;
+            break;}
+        }
+    
+    var oct = Math.floor((25-y)/7);
+    var step = (25-y)%7;
+    
+    switch(step)
+    {
+        case 1: step='C'; break;
+        case 2: step='D'; break;
+        case 3: step='E'; break;
+        case 4: step='F'; break;
+        case 5: step='G'; break;
+        case 6: step='A'; break;
+        case 7: step='B'; oct--; break;
+    }
+    
+    xmlNotes[x]={step:step, octave:oct};
+    
+    if (this.on)xmlNotes[x]=null;
+    this.on = !this.on;
+    this.setFrames(1, (this.on)?1:0, 1);
+    this.frame = (this.on)?1:0;
+    //alert(xmlNotes[x].step+xmlNotes[x].octave);
+    
+}
+
+document.getElementById("XMLgen").onclick = generateXML;
+
+function generateXML()
+
+{
+var xmldoc = document.implementation.createDocument(null, "score-partwise", null);
+//alert(xmldoc2);
+
+for (var i=0; i<xmlNotes.length; i++){
+    
+    var xmlStep = xmldoc.documentElement.appendChild(xmldoc.createElement("step"));
+    xmlStep.textContent = xmlNotes[i].step;
+    var xmlOctave = xmldoc.documentElement.appendChild(xmldoc.createElement("octave"));
+    xmlOctave.textContent = xmlNotes[i].octave;
+}
+alert((new XMLSerializer()).serializeToString(xmldoc));
+};
